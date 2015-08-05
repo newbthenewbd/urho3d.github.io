3e957d5aecd60024aae0901f67beeebe554a5657
urho3d-travis-ci <urho3d.travis.ci@gmail.com>
2015-08-05 03:40:39 +0000

    Travis CI: Emscripten samples update at 2015-08-05 03:40:34 UTC.
    Commit: https://github.com/urho3d/Urho3D/commit/7da3eb5616345135d951b13d260e09fd8bb0bb32
    
    Message: Exposed various kNet connection stats.

--- samples/Urho3D.js
+++ samples/Urho3D.js
@@ -31,7 +31,7 @@ Module.expectedDataFileDownloads++;
                               ((Module['filePackagePrefixURL'] || '') + REMOTE_PACKAGE_BASE);
   
       var REMOTE_PACKAGE_SIZE = 11467464;
-      var PACKAGE_UUID = '87d568bc-770b-4a29-a85f-ceadd3bb9bf8';
+      var PACKAGE_UUID = '6a133a39-fc26-462d-a705-9f93bbfcc363';
     
     function fetchRemotePackage(packageName, packageSize, callback, errback) {
       var xhr = new XMLHttpRequest();
